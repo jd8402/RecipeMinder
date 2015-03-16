be03196a91af9f958a97aad41c75f8ffd2bea5a9
Jessica Deaton <jd8402@hotmail.com>
2015-03-15 21:43:30 -0500

    Full Project Load - App is fleshed out, bootstrap has been added

--- recipeminder/settings.py
+++ recipeminder/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'recipes',
+    'bootstrap3',
+
 )
 
 MIDDLEWARE_CLASSES = (
@@ -82,3 +85,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
+
+BOOTSTRAP3 = {
+    'theme_url': 'http://bootswatch.com/amelia/bootstrap.min.css'
+}
--- recipeminder/urls.py
+++ recipeminder/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
+import recipes
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'recipeminder.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^recipes/', include('recipes.urls')),
 )
